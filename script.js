b9c9363aecce80324a86e01b3cba7026a6f5472e
SuperWebMonkey <vikingsattack@outlook.com>
2024-09-17 18:57:20 -0700

    change in ary code

--- script.js
+++ script.js
@@ -1,124 +1,122 @@
 console.log("R-ALAB");
 
 // const csvString =
-//   'ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor's Assistant,26';
+//   "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor's Assistant,26";
 
-const csvString =
-  "Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232";
+// // const csvString =
+// //   "Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232";
 
-let cell1 = "";
-let cell2 = "";
-let cell3 = "";
-let cell4 = "";
+// let cell1 = "";
+// let cell2 = "";
+// let cell3 = "";
+// let cell4 = "";
 
-let currentCell = 1;
+// let currentCell = 1;
 
-for (let i = 0; i < csvString.length; i++) {
-  //checking for new cell
-  if (csvString[i] === ",") {
-    currentCell++;
-    continue;
-  }
-  // checking for a new row
-  if (csvString[i] === "\n") {
-    cell1 = "";
-    cell2 = "";
-    cell3 = "";
-    cell4 = "";
-    currentCell = 1;
-    continue;
-  }
+// for (let i = 0; i <= csvString.length; i++) {
+//   //checking for new cell
+//   if (csvString[i] === ",") {
+//     currentCell++;
+//     continue;
+//   }
+//   // checking for a new row
+//   if (csvString[i] === "\n") {
+//     cell1 = "";
+//     cell2 = "";
+//     cell3 = "";
+//     cell4 = "";
+//     currentCell = 1;
+//     continue;
+//   }
 
-  switch (currentCell) {
-    case 1:
-      cell1 += csvString[i];
-      break;
-    case 2:
-      cell2 += csvString[i];
-      break;
-    case 3:
-      cell3 += csvString[i];
-      break;
-    case 4:
-      cell4 += csvString[i];
-      break;
+//   switch (currentCell) {
+//     case 1:
+//       cell1 += csvString[i];
+//       break;
+//     case 2:
+//       cell2 += csvString[i];
+//       break;
+//     case 3:
+//       cell3 += csvString[i];
+//       break;
+//     case 4:
+//       cell4 += csvString[i];
+//       break;
 
-    default:
-      console.log(`cell${currentCell} doesn't exist`);
-      break;
-  }
+//     default:
+//       console.log(`cell${currentCell} doesn't exist`);
+//       break;
+//   }
 
-  if (
-    (currentCell === 4 && csvString[i + 1] === "\n") ||
-    i + 1 === csvString.length
-  ) {
-    console.log(cell1, cell2, cell3, cell4);
-  }
-}
+//   if (
+//     (currentCell === 4 && csvString[i + 1] === "\n") ||
+//     i + 1 === csvString.length
+//   ) {
+//     console.log(cell1, cell2, cell3, cell4);
+//   }
+// }
 
 // first part
-console.log("part 1");
-const col = [];
+// console.log("part 1");
 
-// Count the length based on the number of commas
-let length = 0;
-let countCommas = 1;
+// const col = [];
 
-for (let i = 0; i < csvString.length; i++) {
-  // break out of loop when newline found
-  if (csvString[i] === "\n") {
-    break;
-  }
+// // Count the length based on the number of commas
+// let length = 0;
+// let countCommas = 1;
 
-  if (csvString[i] === ",") {
-    countCommas++;
-  }
-}
+// for (let i = 0; i < csvString.length; i++) {
+//   // break out of loop when newline found
+//   if (csvString[i] === "\n") {
+//     break;
+//   }
 
-console.log("Number of commas", countCommas);
+//   if (csvString[i] === ",") {
+//     countCommas++;
+//   }
+// }
 
-length = countCommas;
+// console.log("Number of commas", countCommas);
 
-console.log("length is", length);
+// length = countCommas;
 
-// Set the number of columns
-for (let i = 0; i < length; i++) {
-  col[i] = "";
-}
+// console.log("length is", length);
 
-console.log(col);
+// // Set the number of columns
+// for (let i = 0; i < length; i++) {
+//   col[i] = "";
+// }
+
+// console.log(col);
 
 // Store result in two dimensional array
-let mainAry = [];
+// Split the columns using the split function
+console.log("\nLoop for adding subary to mainary");
+const csvString =
+  "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor's Assistant,26";
+
+let mainAry = []; // main ary that will contain the sub arrays
 let i = 0;
+let cell = ""; // cell of the table of data
+let subAry = [];
 
-// infinite loop - debug later
-// subary for every newline
-while (i < csvString.length) {
-  let col = "";
-  let subAry = [];
-  while (csvString[i] !== "\n") {
-    if (csvString[i] === ",") {
-      // console.log('column:', col);
-      subAry.push(col);
-      continue;
+const splitAry = csvString.split("\n");
+// console.log(splitAry);
+
+for (let i = 0; i < splitAry.length; i++) {
+  let str = splitAry[i];
+  console.log(str);
+  for (let j = 0; j <= str.length; j++) {
+    if (str[j] !== ",") {
+      // console.log(str[j]);
+      cell += str[j];
     } else {
-      col += csvString[i];
-    }
-
-    // case needed since the end of the string does not have a \n
-    if (i === csvString.length) {
-      break;
+      // console.log(cell);
+      subAry.push(cell);
+      cell = "";
     }
   }
-  mainAry.push(subAry);
-  i++;
+  // console.log(str);
+  // console.log(subAry);
+  subAry = [];
 }
-
-const obj = {
-  id: 42,
-  // name:
-  cell: col,
-};
-
-console.log("object:", obj);
